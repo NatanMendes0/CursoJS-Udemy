8cc0c07a75c745df3fc57b9732332ac1666d62b3
Natan <natanmendes.gr062@academico.ifsul.edu.br>
2022-06-01 09:44:30 -0300

    primeiros códigos js

---
+++ aula 3/exercicios/index.js
@@ -0,0 +1,4 @@
+nome = "Natan";
+hora = 10;
+
+console.log('Meu nome é ' + nome + '. Estou aprendendo Javascript às ' + hora + ' horas.');
---
+++ aula 3/index.js
@@ -0,0 +1,6 @@
+console.log('Luiz "Otavio"'); //String
+console.log(`'Luiz' "Otavio"`);
+
+console.log(123); //Number
+
+console.log(35, 15.38, 'Luiz'); //Multiple values
